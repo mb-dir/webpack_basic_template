4c3421ec7f57a42d3eeb21434dbe9914328a70ca
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-22 21:29:02 +0100

    Prepare prod config
    Prepare prod config for merge with common config - now there are things
    required on prod config only

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,41 +1,11 @@
+const { merge } = require('webpack-merge');
+const common = require("./webpack.common.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = merge(common, {
     mode: "production",
-    entry: {
-        index: "./src/modules/index.js",
-        //You can add here more entrypoints
-    },
-    output: {
-        filename: "[name].[contenthash:8].js",
-        path: __dirname + "/dist",
-    },
-
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-
-          {
-            test: /\.s[ac]ss$/i,
-            use: ["style-loader", "css-loader", "sass-loader"],
-          },
-        ],
-      },
 
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "./src/pages/index.html",
-        }),
-        new HtmlWebpackPlugin({
-          filename: "subpage.html",
-          template: "./src/pages/subpage.html",
-        }),
-        //You can add here more pages
     ],
-};
+});
