d752fa8c9c90431a56e6812ddf7f99a52a87f1db
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-24 22:01:55 +0100

    Delete rules for styles from commmon
    I have to write separate style's rules for dev and prod config

--- webpack.common.js
+++ webpack.common.js
@@ -10,20 +10,6 @@ module.exports = {
         path: __dirname + "/dist",
     },
 
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-
-          {
-            test: /\.s[ac]ss$/i,
-            use: ["style-loader", "css-loader", "sass-loader"],
-          },
-        ],
-      },
-
     plugins: [
         new HtmlWebpackPlugin({
             filename: "index.html",
