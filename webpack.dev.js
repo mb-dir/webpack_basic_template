8d905225f93924a780db580e750c8173f709a710
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-24 21:18:10 +0100

    Find the problem
    loaders related with styles turn out to be the issue

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,40 +5,40 @@ module.exports = merge(common, {
     devtool: "source-map",
     mode: "development",
 
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          loader: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true,
-              },
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.css$/i,
-          loader: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-      ],
-    },
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.s[ac]ss$/i,
+    //       loader: [
+    //         "style-loader",
+    //         {
+    //           loader: "css-loader",
+    //           options: {
+    //             sourceMap: true,
+    //           },
+    //         },
+    //         {
+    //           loader: "sass-loader",
+    //           options: {
+    //             sourceMap: true,
+    //           },
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       test: /\.css$/i,
+    //       loader: [
+    //         "style-loader",
+    //         {
+    //           loader: "css-loader",
+    //           options: {
+    //             sourceMap: true,
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
     devServer: {
         port: 8080,
         open: true,
