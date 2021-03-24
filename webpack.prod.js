ac097a1504001512a251cdaed88f377a859c7c42
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-03-24 22:03:20 +0100

    Write style's rules for prod
    There is no extra options(sourceMap) in rules

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,19 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
 
+          {
+            test: /\.s[ac]ss$/i,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
+        ],
+    },
     plugins: [
         new CleanWebpackPlugin(),
     ],
